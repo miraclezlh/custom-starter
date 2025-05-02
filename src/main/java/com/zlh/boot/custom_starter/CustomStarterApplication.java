077eb5b49a114b41be15f5b4c8682c02e9747e5e
season <706350462@qq.com>
2025-05-02 11:13:07 +0800

    Init module

---
+++ src/main/java/com/zlh/boot/custom_starter/CustomStarterApplication.java
@@ -0,0 +1,14 @@
+package com.zlh.boot.custom_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+public class CustomStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zlh/boot/custom_starter/CustomStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlh.boot.custom_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
